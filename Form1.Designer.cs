411704bcc0277b2dfdf49ba252c3880aa2fea257
PhilHen99 <112329715+PhilHen99@users.noreply.github.com>
2023-05-12 23:41:12 -0500

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,186 @@
+﻿namespace Login
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lbluser = new System.Windows.Forms.Label();
+            this.lblpass = new System.Windows.Forms.Label();
+            this.txtUsername = new System.Windows.Forms.TextBox();
+            this.txtPassword = new System.Windows.Forms.TextBox();
+            this.btnaccess = new System.Windows.Forms.Button();
+            this.btncancel = new System.Windows.Forms.Button();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.btnupdate = new System.Windows.Forms.Button();
+            this.txtid = new System.Windows.Forms.TextBox();
+            this.lblid = new System.Windows.Forms.Label();
+            this.lblattemptsremaining = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lbluser
+            // 
+            this.lbluser.AutoSize = true;
+            this.lbluser.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbluser.Location = new System.Drawing.Point(36, 42);
+            this.lbluser.Name = "lbluser";
+            this.lbluser.Size = new System.Drawing.Size(184, 25);
+            this.lbluser.TabIndex = 0;
+            this.lbluser.Text = "Ingresa tu usuario";
+            // 
+            // lblpass
+            // 
+            this.lblpass.AutoSize = true;
+            this.lblpass.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblpass.Location = new System.Drawing.Point(36, 120);
+            this.lblpass.Name = "lblpass";
+            this.lblpass.Size = new System.Drawing.Size(220, 25);
+            this.lblpass.TabIndex = 1;
+            this.lblpass.Text = "Ingresa tu contraseña";
+            // 
+            // txtUsername
+            // 
+            this.txtUsername.Location = new System.Drawing.Point(41, 86);
+            this.txtUsername.Name = "txtUsername";
+            this.txtUsername.Size = new System.Drawing.Size(220, 20);
+            this.txtUsername.TabIndex = 4;
+            this.txtUsername.TextChanged += new System.EventHandler(this.txtUsername_TextChanged);
+            // 
+            // txtPassword
+            // 
+            this.txtPassword.Location = new System.Drawing.Point(41, 159);
+            this.txtPassword.Name = "txtPassword";
+            this.txtPassword.Size = new System.Drawing.Size(220, 20);
+            this.txtPassword.TabIndex = 5;
+            // 
+            // btnaccess
+            // 
+            this.btnaccess.Location = new System.Drawing.Point(41, 185);
+            this.btnaccess.Name = "btnaccess";
+            this.btnaccess.Size = new System.Drawing.Size(75, 23);
+            this.btnaccess.TabIndex = 6;
+            this.btnaccess.Text = "Ingresar";
+            this.btnaccess.UseVisualStyleBackColor = true;
+            this.btnaccess.Click += new System.EventHandler(this.btnaccess_Click);
+            // 
+            // btncancel
+            // 
+            this.btncancel.Location = new System.Drawing.Point(181, 185);
+            this.btncancel.Name = "btncancel";
+            this.btncancel.Size = new System.Drawing.Size(75, 23);
+            this.btncancel.TabIndex = 7;
+            this.btncancel.Text = "Cancelar";
+            this.btncancel.UseVisualStyleBackColor = true;
+            this.btncancel.Click += new System.EventHandler(this.btncancel_Click);
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(41, 233);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(75, 23);
+            this.btnAdd.TabIndex = 8;
+            this.btnAdd.Text = "Agregar";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // btnupdate
+            // 
+            this.btnupdate.Location = new System.Drawing.Point(122, 233);
+            this.btnupdate.Name = "btnupdate";
+            this.btnupdate.Size = new System.Drawing.Size(75, 23);
+            this.btnupdate.TabIndex = 9;
+            this.btnupdate.Text = "Actualizar";
+            this.btnupdate.UseVisualStyleBackColor = true;
+            this.btnupdate.Click += new System.EventHandler(this.btnupdate_Click);
+            // 
+            // txtid
+            // 
+            this.txtid.Location = new System.Drawing.Point(170, 17);
+            this.txtid.Name = "txtid";
+            this.txtid.Size = new System.Drawing.Size(40, 20);
+            this.txtid.TabIndex = 10;
+            // 
+            // lblid
+            // 
+            this.lblid.AutoSize = true;
+            this.lblid.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblid.Location = new System.Drawing.Point(36, 12);
+            this.lblid.Name = "lblid";
+            this.lblid.Size = new System.Drawing.Size(133, 25);
+            this.lblid.TabIndex = 11;
+            this.lblid.Text = "Ingresa tu ID";
+            this.lblid.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // lblattemptsremaining
+            // 
+            this.lblattemptsremaining.AutoSize = true;
+            this.lblattemptsremaining.Location = new System.Drawing.Point(178, 217);
+            this.lblattemptsremaining.Name = "lblattemptsremaining";
+            this.lblattemptsremaining.Size = new System.Drawing.Size(106, 13);
+            this.lblattemptsremaining.TabIndex = 12;
+            this.lblattemptsremaining.Text = "Intentos restantes: 0 ";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(309, 280);
+            this.ControlBox = false;
+            this.Controls.Add(this.lblattemptsremaining);
+            this.Controls.Add(this.lblid);
+            this.Controls.Add(this.txtid);
+            this.Controls.Add(this.btnupdate);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.btncancel);
+            this.Controls.Add(this.btnaccess);
+            this.Controls.Add(this.txtPassword);
+            this.Controls.Add(this.txtUsername);
+            this.Controls.Add(this.lblpass);
+            this.Controls.Add(this.lbluser);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lbluser;
+        private System.Windows.Forms.Label lblpass;
+        private System.Windows.Forms.TextBox txtUsername;
+        private System.Windows.Forms.TextBox txtPassword;
+        private System.Windows.Forms.Button btnaccess;
+        private System.Windows.Forms.Button btncancel;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Button btnupdate;
+        private System.Windows.Forms.TextBox txtid;
+        private System.Windows.Forms.Label lblid;
+        private System.Windows.Forms.Label lblattemptsremaining;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,150 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.StartPanel;
+using MySql.Data.MySqlClient;
+using System.Linq.Expressions;
+using System.Data.SqlClient;
+
+namespace Login
+{
+    public partial class Form1 : Form
+    {
+        //public int attemptsLeft = 0;
+        
+        private const string connectionString = "Data Source=PIPO\\SQLEXPRESS;Initial Catalog=Login;Integrated Security=True";
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btncancel_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            ////INSERT///
+            //this imports the table from sql 
+            SqlConnection con = new SqlConnection("Data Source=PIPO\\SQLEXPRESS;Initial Catalog=Login;Integrated Security=True");
+            con.Open();
+            //this takes the info from the database, to prepare them to insert the new values
+            SqlCommand cmd = new SqlCommand("insert into ut values(@id,@user,@password)", con);
+            //this brings the user and password from the form
+            cmd.Parameters.AddWithValue("@id", txtid.Text);
+            cmd.Parameters.AddWithValue("@user", txtUsername.Text);
+            cmd.Parameters.AddWithValue("@password", txtPassword.Text);
+            cmd.ExecuteNonQuery();
+            con.Close();
+
+            MessageBox.Show("Correctamente agregado");
+        }
+
+        private void btnupdate_Click(object sender, EventArgs e)
+        {
+            SqlConnection con = new SqlConnection("Data Source=PIPO\\SQLEXPRESS;Initial Catalog=Login;Integrated Security=True");
+            con.Open();
+            //this updates the info from the db comparing the values individually, with the help of the index (id)
+            SqlCommand cmd = new SqlCommand("update ut set user=@user, password=@password where id=@id", con);
+            //this brings the user and password from the form
+            cmd.Parameters.AddWithValue("@id", txtid.Text);
+            cmd.Parameters.AddWithValue("@user", txtUsername.Text);
+            cmd.Parameters.AddWithValue("@password", txtPassword.Text);
+            cmd.ExecuteNonQuery();
+            con.Close();
+
+            MessageBox.Show("Correctamente actualizado");
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnaccess_Click(object sender, EventArgs e)
+        {
+            {
+                // obtains the username and password
+                string user = txtUsername.Text;
+                string password = txtPassword.Text;
+
+                // establishes the connection with the SQL server
+                string connectionString = "Data Source=PIPO\\SQLEXPRESS;Initial Catalog=Login;Integrated Security=True";
+                using (SqlConnection connection = new SqlConnection(connectionString))
+                {
+                    try
+                    {
+                        connection.Open();
+
+                        // creates an sql query to obtain the login data from the user
+                        string query = "SELECT COUNT(*) FROM ut WHERE user=@user AND password=@password";
+                        SqlCommand command = new SqlCommand(query, connection);
+                        command.Parameters.AddWithValue("@user", user);
+                        command.Parameters.AddWithValue("@password", password);
+
+                        // executes the sql query and obtains the number of affected rows 
+                        int result = (int)command.ExecuteScalar();
+
+                        // if the amount of affected rows is greater tan 0, the login is sucessfull 
+                        if (result > 0)
+                        {
+                            MessageBox.Show ("Inicio de sesión correcto");
+                        }
+                        // If the number of affected rows is cero, the credentials are incorrect 
+                        else
+                        {
+                            // Decrease the count
+                            int remainingAttempts = int.Parse(lblattemptsremaining.Text) - 1;
+                            lblattemptsremaining.Text = remainingAttempts.ToString();
+
+                            // shows a message when the counter reaches zero
+                            if (remainingAttempts == 0)
+                            {
+                                MessageBox.Show ("El inicio de sesión falló y no quedan más intentos.");
+                                btnaccess.Enabled = false;
+                            }
+                            // if it's not zero, shows a warning error.
+                            else
+                            {
+                                MessageBox.Show ("El inicio de sesión falló. Quedan " + remainingAttempts.ToString() + " intentos.");
+                            }
+                        }
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show ("Error al intentar iniciar sesión: " + ex.Message);
+                    }
+                }
+            }
+
+        }
+
+        private void txtUsername_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
+
+                   
+
+
+        
+    
+
